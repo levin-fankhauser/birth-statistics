77ffb7c64a2dc1890243f5cdb9358860cdbaaa6b
levin-fankhauser <levin.fankhauser@gmx.ch>
2024-10-08 11:38:01 +0200

    Temporary changes to log out possible values in a list entry

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import models.BirthRecord;
 import services.BirthRecordService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -9,6 +10,13 @@ public class Main {
 		BirthRecordService service = new BirthRecordService("data.json");
 
 		List<BirthRecord> filteredRecords = service.getAll();
-		filteredRecords.forEach(System.out::println);
+		List<String> listItem = new ArrayList<>();
+		filteredRecords.forEach(item -> {
+			if (!listItem.contains(item.name_citizenship_bfs())) {
+				listItem.add(item.name_citizenship_bfs());
+			}
+		});
+		listItem.forEach(System.out::println);
+		System.out.println("\nAmount of possibilities: " + listItem.size());
 	}
 }
